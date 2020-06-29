163b605479f9ac8e532bf267812a111a361d83e9
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-06-29 18:56:35 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -14,6 +14,6 @@ def toadstool
  end
 
   def all_phrases
-    catch_phrase = 'mario, toadstool, and link'
-   puts catch_phrase
+    all_phrase = 'mario, toadstool, and link'
+   puts all_phrase
  end 

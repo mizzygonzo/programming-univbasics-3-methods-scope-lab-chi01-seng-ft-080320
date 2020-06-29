55ae6fc06f2d146b1ac6a798d4e2bcbc67b2938d
MaryElizabeth Gonzales <xeligon@gmail.com>
2020-06-29 18:32:11 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -4,9 +4,8 @@ def mario
 end 
 
 def toadstool
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts status
-end
-
-def link 
-  
+   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+   puts status
+ end
+ 
+ 
